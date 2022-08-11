7dc3b41e3ae3283d2dab87ae71f3aaaedc975a0e
ABHISHEKBARNWAL1301 <77736997+ABHISHEKBARNWAL1301@users.noreply.github.com>
2022-08-11 10:23:02 +0530

    frontend changes

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -23,7 +23,7 @@ function About() {
                 <div className="row">
                     <div className="col-md-6 d-flex flex-column justify-content-center">
                         <h2 className="mb-3" style={{ fontWeight: "Bold" }}>Make something <span style={{ color: "#9C27B0" }}>Awesome</span> </h2>
-                        <p>Dropbox is made from the pain of writing all the things in notebook which is very hectic :(, So we mad an online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee.
+                        <p>Dropbox is made from the pain of writing all the things in notebook which is very hectic, So we mad an online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee.
                             you can also access your notes anywhere in your world, at anytime time . So dont forget to Create note because creating anything is always important
                         </p>
                         <div className="d-flex justify-content-center mt-3">
@@ -42,8 +42,8 @@ function About() {
                     <div className="col-md-6 d-flex flex-column justify-content-center">
                         <h2 className="mb-3" style={{ fontWeight: "Bold" }}>Powering the <span style={{ color: "#9C27B0" }}>internet’s visuals</span> </h2>
                         <p>
-                            How we started? The concept was simple. iNotebook was born from the pain of writing all the things in notebook which is very hectic :( .
-                            An online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee
+                            How we started? The concept was simple. Dropbox was born from the pain of writing all the things in notebook which is very hectic .
+                            An online web platform where you can create, edit, upload, delete your notes privately and securely without any disturbance
                         </p>
                         <div className="d-flex justify-content-center mt-3">
                             <Button component={Link} to="/register" variant="contained" color="secondary" style={{ color: "White", textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "1.3rem" }}>Sign up now</Button>
@@ -56,7 +56,7 @@ function About() {
                 <div className="content">
                     <div className="top">
                         <div className="logo-details">
-                            <span className="logo_name"><span style={{ color: "#9C27B0" }}>i</span>Notebook</span>
+                            <span className="logo_name">Dropbox</span>
                         </div>
                         <div className="media-icons">
                             <Link to="/"><i className="fab fa-facebook-f"></i></Link>
@@ -71,7 +71,6 @@ function About() {
                             <li className="link_name">Company</li>
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/new">New Notes</Link></li>
-                            <li><Link to="/about">About us</Link></li>
                             <li><Link to="/">Get started</Link></li>
                         </ul>
                         <ul className="box">
@@ -92,7 +91,7 @@ function About() {
                             <li><Link to="c/61554c63dfd6a37d71449b5c">Survivalist Notes</Link></li>
                         </ul>
                         <ul className="box input-box">
-                            <li className="link_name">About iNotebook</li>
+                            <li className="link_name">About Dropbox</li>
                             <li style={{color: "#F7FFFF"}}>
                             An online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee
                             </li>
@@ -101,7 +100,7 @@ function About() {
                 </div>
                 <div className="bottom-details">
                     <div className="bottom_text">
-                        <span className="copyright_text">Copyright © 2021 <Link to="/">iNotebook</Link>All rights reserved</span>
+                        <span className="copyright_text">Copyright © 2022 <Link to="/">Dropbox</Link>All rights reserved</span>
                         <span className="policy_terms">
                             <Link to="/">Privacy policy</Link>
                             <Link to="/">Terms & condition</Link>
